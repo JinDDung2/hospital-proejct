5658afe6618345e0ea8681e3d2e29f0e8e8e8b27
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-13 13:21:26 +0900

    id, 주소 파싱 후 테스트

--- src/main/java/likelion/Parser/HospitalParser.java
+++ src/main/java/likelion/Parser/HospitalParser.java
@@ -4,12 +4,11 @@ package likelion.Parser;
 import likelion.hospital.Hospital;
 public class HospitalParser implements Parser<Hospital> {
 
-    private String replaceAllOut(String str) {
-        return str.replaceAll("\"", "");
-    }
     @Override
     public Hospital parse(String str) {
+        str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
-        return new Hospital(replaceAllOut(splitted[0]),replaceAllOut(splitted[1]));
+        // 0:id, 1:address
+        return new Hospital(splitted[0], splitted[1]);
     }
 }
