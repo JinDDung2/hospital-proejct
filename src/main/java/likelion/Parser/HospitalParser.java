6280417f22587ec72f631ad9c8bf211336dd5b8d
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-13 13:17:56 +0900

    id, 주소 파싱 후 테스트

--- src/main/java/likelion/Parser/HospitalParser.java
+++ src/main/java/likelion/Parser/HospitalParser.java
@@ -2,11 +2,14 @@ package likelion.Parser;
 
 
 import likelion.hospital.Hospital;
-
 public class HospitalParser implements Parser<Hospital> {
+
+    private String replaceAllOut(String str) {
+        return str.replaceAll("\"", "");
+    }
     @Override
     public Hospital parse(String str) {
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0]);
+        return new Hospital(replaceAllOut(splitted[0]),replaceAllOut(splitted[1]));
     }
 }
--- src/main/java/likelion/hospital/Hospital.java
+++ src/main/java/likelion/hospital/Hospital.java
@@ -18,11 +18,36 @@ public class Hospital{
     private String name;
     private String subdivision;
 
-    public Hospital(String id) {
-        this.id = id.replaceAll("\"", "");
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getEmergency_room() {
+        return emergency_room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubdivision() {
+        return subdivision;
     }
 }
