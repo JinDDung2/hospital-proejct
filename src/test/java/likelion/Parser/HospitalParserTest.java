c2dea1bc20842506aa4ed117a4bcb18248c66e23
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-14 13:07:20 +0900

    getSqlInsertQuery2 테스트

--- src/test/java/likelion/Parser/HospitalParserTest.java
+++ src/test/java/likelion/Parser/HospitalParserTest.java
@@ -75,4 +75,20 @@ class HospitalParserTest {
         assertEquals(sql, hospital.getSqlInsertQuery());
     }
 
+    @Test
+    void makeSqlQueryTest2() {
+        HospitalParser hospitalParser = new HospitalParser();
+        Hospital hospital = hospitalParser.parse(this.line1);
+        String sql = "INSERT INTO `likelion-db`.`seoul_hospital`\n" +
+                "(`id`,`address`,`district`,`category`,`emergency_room`,`name`,`subdivision`)\n"+
+                "VALUES\n" +
+                "(\"A1120837\"," + "\"서울특별시 금천구 벚꽃로 286 삼성리더스타워 111~114호 (가산동)\"," +
+                "\"서울특별시 금천구\"," +
+                "\"C\"," +
+                "2," +
+                "\"가산기대찬의원\"," +
+                "\"의원\");\n";
+        assertEquals(sql, hospital.getSqlInsertQuery2());
+    }
+
 }
