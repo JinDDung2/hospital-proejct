f24649d017bc33ab3a7cd5ad4fff7639dc5d9adc
jinhyuck <gjwlsgur4866@gmail.com>
2022-10-17 09:03:21 +0900

    Main 추가

--- src/main/java/likelion/Main.java
+++ src/main/java/likelion/Main.java
@@ -18,6 +18,7 @@ public class Main {
         LineReader<Hospital> hospitalLineReader = new LineReader<>(new HospitalParser());
         String fileName = "/Users/jinhyuck/Downloads/seoul_hospital_info.csv";
         List<Hospital> hospitals = hospitalLineReader.readLines(fileName);
+        
 
         System.out.println(hospitals.size());
         FileWriter writer = new FileWriter();
